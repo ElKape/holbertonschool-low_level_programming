7a30d48d8d1d80bc085e7d15456692ef6e586670
ElKape <damian.rau.kp@gmail.com>
2024-06-14 18:30:49 +0000

    Thanks Betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * variables_if_else_while - if
+ * Positive anything is better than negative nothing
  * Return: 0
 */
 
 int main(void)
 {
 	int n;
+	int u_dig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	u_dig = n%10;
 		if (n > 5)
-			printf("Last digit of %i and is greater than 5\n", n);
+			printf("Last digit of %i and %i is greater than 5\n", n, u_dig);
 		if (n == 0)
-			printf("Last digit of %i and is 0\n", n);
+			printf("Last digit of %i and %i is 0\n", n, u_dig);
 		if (n < 6)
-			printf("Last digit of %i and is less than 6 and not 0\n", n);
+			printf("Last digit of %i and %i is less than 6 and not 0\n", n, u_dig);
 
 	return (0);
 }
