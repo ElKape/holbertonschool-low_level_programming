208d632ac6cb6ab30471ef3e5459616dd27b6ec5
ElKape <damian.rau.kp@gmail.com>
2024-06-17 18:07:42 +0000

    Betty, me querés matar?

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -  Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
