da459ff7c2f87f6866a796b9fd8b1e875de13da0
ElKape <damian.rau.kp@gmail.com>
2024-06-18 18:03:22 +0000

    Betty, me querés matar?

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -19,6 +19,13 @@ void times_table(void)
 			_putchar(' ');
 
 			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
