d9aaf0fe095f4c170bbc4785cb65a34e14467a2a
ElKape <damian.rau.kp@gmail.com>
2024-06-18 12:18:27 +0000

    Betty, me querés matar?

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * __isalpha - Function that checks for alphabetic character.
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+
+int __isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
