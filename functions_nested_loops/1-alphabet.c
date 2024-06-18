db2e3c985bd7d7b6d3edb5d67e1e64bd7ff2ea5c
ElKape <damian.rau.kp@gmail.com>
2024-06-18 11:20:24 +0000

    Betty, me querés matar?

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main -  Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * main -  Write a function that prints the alphabet,
+ * in lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char c = 'a';
+char c = 'a';
 	while (c <= 'z')
 	{
 		_putchar(c);
