af2b6a81ea23ef26cf3edaf81a0031ce12ab0282
ElKape <damian.rau.kp@gmail.com>
2024-06-18 20:32:04 +0000

    Betty, me querés matar?

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,21 +10,26 @@
 
 void print_to_98(int n)
 {
-	for ( ; n <= 98; n++)
-	{
-		if (n == 98)
-			printf("%i", n);
-		else
-			printf("%i, ", n);
+	if (n < 98)
+	{	
+		for ( ; n <= 98; n++)
+		{
+			if (n == 98)
+				printf("%i", n);
+			else
+				printf("%i, ", n);
+		}
+		printf("\n");
 	}
-	printf("\n");
-
-	for ( ; n >= 98; n--)
+	else
 	{
-		if (n == 98)
-			printf("%i", n);
-		else
-			printf("%i, ", n);
+		for ( ; n >= 98; n--)
+		{
+			if (n == 98)
+				printf("%i", n);
+			else
+				printf("%i, ", n);
+		}
+		printf("\n");
 	}
-	printf("\n");
 }
