910dc94e386f47a7adfc317dceab4026543393c0
ElKape <damian.rau.kp@gmail.com>
2024-06-18 15:37:21 +0000

    Betty, me querés matar?

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,2 +1,24 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int hora, minuto;
+
+	for (hora = 0; hora <= 23; hora++)
+	{
+		for (minuto = 0; minuto <= 59; minuto++)
+		{
+			_putchar((hora / 10) + '0');
+			_putchar((hora / 10) + '0');
+			_putchar(':');
+			_putchar((minuto / 10) + '0');
+			_putchar((minuto / 10) + '0');
+			_putchar('\n');
+		}
+}
