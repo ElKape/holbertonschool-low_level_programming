2a64db0c79b3dc39e10d2c1c8a8d4ad908fb759c
ElKape <damian.rau.kp@gmail.com>
2024-06-19 19:38:31 +0000

    Betty, me querés matar?

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,12 @@ void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
+	for (x = 0; x < n; x++)
 	{
-		for (x = 0; x < n; x++)
+		_putchar('_');
 		{
-			_putchar('_');
+			if (n <= 0)
+			_putchar('\n');
 		}
-	}
 	_putchar('\n');
 }
