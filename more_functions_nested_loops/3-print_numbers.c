258603cc2769411c9960a1694928bd560de8eaaa
ElKape <damian.rau.kp@gmail.com>
2024-06-19 18:20:34 +0000

    Betty, me querés matar?

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Function that that prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - Function that that prints the numbers, from 0 to 9,
+ * followed by a new line.
  *
  * Return: null.
  */
