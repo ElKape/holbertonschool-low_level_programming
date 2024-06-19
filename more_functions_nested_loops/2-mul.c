f197fcc9c629a5c1254e2cb49f14f282e007d1be
ElKape <damian.rau.kp@gmail.com>
2024-06-19 17:59:24 +0000

    Betty, me querés matar?

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int mul - Function that multiplies two integers.
+ *
+ * @a: is a num.
+ * @b: is a num.
+ * Return: the result of a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: null.
+ */
+
+void print_numbers(void)
+{
+	int n;
+	if (n <= 0 && >= 9)
+	{
+		_putchar('n')
+	}
+	else
+	_putchar('\n')
+	return (0);
+}
