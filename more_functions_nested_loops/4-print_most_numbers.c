68e160eec2d102ee8f5d10e5b39a6a001a02932b
ElKape <damian.rau.kp@gmail.com>
2024-06-19 19:15:21 +0000

    Betty, me querés matar?

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Function that prints the numbers, from 0 to 9, followed
- * by a new line.
+ * print_most_numbers - Function that prints the numbers,
+ * from 0 to 9, followed by a new line.
  *
  * Return: null.
  */
 
 void print_most_numbers(void)
 {
-	int n;
-	while (n != 2 || n != 4)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (n = 0; n <= 9; n++)
-		_putchar(n);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
