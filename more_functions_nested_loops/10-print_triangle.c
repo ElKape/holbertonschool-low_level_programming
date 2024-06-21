6fd1d0eeeeeb0295a72b666cdd20eec65175e099
ElKape <damian.rau.kp@gmail.com>
2024-06-21 13:57:01 +0000

    Betty, me querés matar?

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,6 @@
 void print_triangle(int size)
 {
 	int i, j;
-
 	if (size <= 0)
 		_putchar('\n');
 	else
@@ -19,9 +18,9 @@ void print_triangle(int size)
 			for (j = 1; j <= size - i; j++)
 			_putchar(' ');
 		}
-		{
 		for (j = 1; j <= i; j++)
-		_putchar('#');
+		{
+			_putchar('#');
 		}
-	_putchar('\n');
+		_putchar('\n');
 }
